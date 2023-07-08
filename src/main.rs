2834e3dad020229ac788ba836a29bb78b0619586
Powei Lin <poweilin1994@gmail.com>
2023-07-08 15:31:07 -0400

    mx g

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod example_cost;
+use std::cmp;
 // use std::mem::MaybeUninit;
 
 use example_cost::cost_trait::CostFunc;
 use example_cost::ExampleStatic;
 use faer_core::Mat;
+use faer_core::zip;
 // use faer_lu::partial_pivoting;
 // use std::ops;
 
@@ -28,12 +30,23 @@ fn main() {
     ExampleStatic::apply(&x0, &mut residual, Some(&mut jac));
     let H = jac.transpose() * jac.clone();
     let g_ = jac.transpose() * residual.clone();
-
     println!("residual \n{:?}\n", residual);
     println!("jac \n{:?}\n", &jac);
     println!("H:\n{:?}", H);
     println!("gradient:\n{:?}", g_);
+    let mut max_gradient: f64 = 0.0;
+    for i in 0..g_.ncols(){
+        for j in 0..g_.nrows(){
+            unsafe{
+                max_gradient = max_gradient.max(g_.read_unchecked(j, i).abs());
+            }
+        }
+    }
 
+    // let a: [f64: 3] = g_.into();
+
+    println!("{}", max_gradient);
+    // let mm: PermutationRef = g_.into();
 
     // const NUM_PARAMETERS: u32 = 3;
     // const NUM_RESIDUALS: u32 = 2;
