05461e895bc2647e6cc696ceea218abdb26e4dc1
Powei Lin <poweilin1994@gmail.com>
2023-07-09 16:28:26 -0400

    new files

---
+++ src/cost_function.rs
@@ -0,0 +1,18 @@
+extern crate nalgebra as na;
+
+use na::SMatrix;
+pub trait CostFunc<T, const NUM_PARAMETERS: usize, const NUM_RESIDUALS: usize> {
+    fn apply(
+        _params: &SMatrix<T, NUM_PARAMETERS, 1>,
+        _residual: &mut SMatrix<T, NUM_RESIDUALS, 1>,
+        _jacobian: Option<&mut SMatrix<T, NUM_RESIDUALS, NUM_PARAMETERS>>,
+    ) {
+        unimplemented!()
+    }
+    fn num_params() -> usize {
+        NUM_PARAMETERS
+    }
+    fn num_residuals() -> usize {
+        NUM_RESIDUALS
+    }
+}
---
+++ src/tiny_solver.rs
@@ -0,0 +1,5 @@
+// use crate::cost_function;
+
+// pub struct TinySolver{
+
+// }
