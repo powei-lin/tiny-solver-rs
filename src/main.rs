3762f46a00fc43c3614b161d202230f5a66ae733
Powei Lin <poweilin1994@gmail.com>
2023-07-22 23:03:24 -0400

    auto grad works

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 extern crate nalgebra as na;
-use num_dual;
+use na::*;
+use nalgebra::allocator::Allocator;
+use num_dual::*;
+// use num_dual::{jacobian, DualSVec64, DualNum};
 
 fn f<D: num_dual::DualNum<f64>>(x: D, y: D) -> D {
-    x.powd(x.clone()) + y
+    // x.powd(x.clone()) + y
+    x.powi(3) * y.powi(2)
 }
 
-fn f2<D: num_dual::DualNum<f64>>(xvec: &na::SMatrix<D, 3, 1>) -> na::SMatrix<D, 2, 1> {
-    let x = xvec[(0, 0)].clone();
-    let y = xvec[(1, 0)].clone();
-    let z = xvec[(2, 0)].clone();
-    na::SMatrix::<D, 2, 1>::new(x + y.clone().mul(2.0) + z.clone().mul(4.0), y * z)
+fn f2(xy: na::SVector<num_dual::DualSVec64<3>, 3>) -> na::SVector<num_dual::DualSVec64<3>, 2> {
+    return na::SVector::from([
+                         xy[0] * xy[1].powi(3) * xy[2],
+                         xy[0].powi(2) * xy[1] * xy[2].powi(2),
+                        ]);
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
 
 fn main() {
@@ -21,33 +29,30 @@ fn main() {
     // let residual = mat![[x + 2.0 * y + 4.0 * z], [y * z]];
     // let jacobian = mat![[1.0, 2.0, 4.0], [0.0, z, y]];
     // println!("{}", jacobian.ncols());
-    let xvec = na::SMatrix::<num_dual::Dual64, 3, 1>::new(
-        num_dual::Dual::from(1.0),
-        num_dual::Dual::from(1.0),
-        num_dual::Dual::from(1.0),
-    );
-    let out = f2(&xvec);
-    println!("{xvec}");
-    println!("{out}");
-    // let (x, y) = (5.0, 4.0);
-    // // Calculate a simple derivative using dual numbers
-    // let x_dual = num_dual::Dual64::from(x).derivative();
-    // let y_dual = num_dual::Dual64::from(y);
-    // println!("{}", f(x_dual, y_dual)); // 2000 + [1200]ε
-
-    // // // or use the provided function instead
-    // let (_, df) = num_dual::first_derivative(|x| f(x, y.into()), x);
-    // println!("{df}"); // 1200
-
-    // // Calculate a gradient
-    // let (value, grad) = gradient(|v| f(v[0], v[1]), SMatrix::from([x, y]));
-    // println!("{value} {grad}"); // 2000 [1200, 1000]
-
-    // // Calculate a Hessian
-    // let (_, _, hess) = hessian(|v| f(v[0], v[1]), SMatrix::from([x, y]));
-    // println!("{hess}"); // [[480, 600], [600, 250]]
-
-    // // for x=cos(t) and y=sin(t) calculate the third derivative w.r.t. t
-    // let (_, _, _, d3f) = third_derivative(|t| f(t.cos(), t.sin()), 1.0);
-    // println!("{d3f}"); // 7.358639755305733
+    // let xvec = na::SMatrix::<num_dual::Dual64, 3, 1>::new(
+    //     num_dual::Dual::from(1.0),
+    //     num_dual::Dual::from(1.0),
+    //     num_dual::Dual::from(1.0),
+    // );
+    let xy = na::SVector::from([5.0, 3.0, 2.0]);
+    // let fun = |xy: na::SVector<num_dual::DualSVec64<3>, 3>| {
+    //     na::SVector::from([
+    //         xy[0] * xy[1].powi(3) * xy[2],
+    //         xy[0].powi(2) * xy[1] * xy[2].powi(2),
+    //     ])
+    // };
+    let (f, jac) = num_dual::jacobian(f2, xy);
+    println!("{f} {jac}");
+    println!("{}", f2(xy.map(DualVec::from_re)));
+    print_type_of(&jac);
+    let mm = na::SMatrix::<f64, 2, 3>::new(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+    print_type_of(&mm);
+    assert_eq!(f[0], 270.0); // xy³z
+    assert_eq!(f[1], 300.0); // x²yz²
+    assert_eq!(jac[(0, 0)], 54.0); // y³z
+    assert_eq!(jac[(0, 1)], 270.0); // 3xy²z
+    assert_eq!(jac[(0, 2)], 135.0); // xy³
+    assert_eq!(jac[(1, 0)], 120.0); // 2xyz²
+    assert_eq!(jac[(1, 1)], 100.0); // x²z²
+    assert_eq!(jac[(1, 2)], 300.0); // 2x²yz
 }
--- src/tiny_solver.rs
+++ src/tiny_solver.rs
@@ -61,20 +61,21 @@ pub trait TinySolverF64<const NUM_PARAMETERS: usize, const NUM_RESIDUALS: usize>
 pub trait TinySolver<const NUM_PARAMETERS: usize, const NUM_RESIDUALS: usize> {
     const NUM_PARAMETERS: usize = NUM_PARAMETERS;
     const NUM_RESIDUALS: usize = NUM_RESIDUALS;
-    fn cost_function<D: num_dual::DualNum<f64>>(
-        _params: &mut na::SVector<D, NUM_PARAMETERS>,
-        _residual: &mut na::SMatrix<D, NUM_RESIDUALS, 1>,
-    ) {
-        unimplemented!()
-    }
-    fn solve(params: &mut na::SMatrix<f64, NUM_PARAMETERS, 1>) -> bool {
+
+    fn cost_function(
+        _params: na::SVector<num_dual::DualSVec64<NUM_PARAMETERS>, NUM_PARAMETERS>
+    ) -> na::SVector<num_dual::DualSVec64<NUM_PARAMETERS>, NUM_RESIDUALS>;
+
+    fn solve(params: &mut na::SVector<f64, NUM_PARAMETERS>) -> bool {
         let mut status = Status::defualt();
         let mut u: f64;
         let mut v = 2;
-        let mut residual = na::SMatrix::<f64, NUM_RESIDUALS, 1>::zeros();
-        let mut jac = na::SMatrix::<f64, NUM_RESIDUALS, NUM_PARAMETERS>::zeros();
+
+        // println!("{f} {jac}");
+        // println!("{}", f2(xy.map(DualVec::from_re)));
+
         for i in 0..status.max_iterations {
-            Self::cost_function(params, &mut residual);
+            let (residual, jac) = num_dual::jacobian(Self::cost_function, params.clone());
             let g_ = jac.transpose() * -residual.clone();
             let H = jac.transpose() * jac.clone();
             println!("residual \n{}\n", residual);
@@ -112,26 +113,3 @@ pub trait TinySolver<const NUM_PARAMETERS: usize, const NUM_RESIDUALS: usize> {
         true
     }
 }
-
-//     let (x, y) = (5.0, 4.0);
-//     // Calculate a simple derivative using dual numbers
-//     let x_dual = Dual64::from(x).derivative();
-//     let y_dual = Dual64::from(y);
-//     println!("{}", f(x_dual, y_dual)); // 2000 + [1200]ε
-
-//     // or use the provided function instead
-//     let (_, df) = first_derivative(|x| f(x, y.into()), x);
-//     println!("{df}"); // 1200
-
-//     // Calculate a gradient
-//     let (value, grad) = gradient(|v| f(v[0], v[1]), SMatrix::from([x, y]));
-//     println!("{value} {grad}"); // 2000 [1200, 1000]
-
-//     // Calculate a Hessian
-//     let (_, _, hess) = hessian(|v| f(v[0], v[1]), SMatrix::from([x, y]));
-//     println!("{hess}"); // [[480, 600], [600, 250]]
-
-//     // for x=cos(t) and y=sin(t) calculate the third derivative w.r.t. t
-//     let (_, _, _, d3f) = third_derivative(|t| f(t.cos(), t.sin()), 1.0);
-//     println!("{d3f}"); // 7.358639755305733
-// }
