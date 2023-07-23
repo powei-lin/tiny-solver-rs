dd6943f26fdaefa2e097da4282b18b5f5f67d4f0
Powei Lin <poweilin1994@gmail.com>
2023-07-23 15:03:18 -0400

    change to using autograd

--- examples/example_solve.rs
+++ examples/example_solve.rs
@@ -1,28 +1,23 @@
-use tiny_solver_rs::tiny_solver::TinySolverF64;
+use std::ops::Mul;
 
-pub type Matrix2x1d = nalgebra::SMatrix<f64, 2, 1>;
+use tiny_solver_rs::tiny_solver::TinySolver;
+
 pub type Matrix3x1d = nalgebra::SMatrix<f64, 3, 1>;
-pub type Matrix2x3d = nalgebra::SMatrix<f64, 2, 3>;
 pub struct ExampleStatic {}
 
-impl TinySolverF64<3, 2> for ExampleStatic {
+impl TinySolver<3, 2> for ExampleStatic {
     fn cost_function(
-        params: &mut nalgebra::SMatrix<f64, { Self::NUM_PARAMETERS }, 1>,
-        residual: &mut nalgebra::SMatrix<f64, { Self::NUM_RESIDUALS }, 1>,
-        jacobian: Option<
-            &mut nalgebra::SMatrix<f64, { Self::NUM_RESIDUALS }, { Self::NUM_PARAMETERS }>,
-        >,
-    ) {
+            params: nalgebra::SVector<num_dual::DualSVec64<3>, 3>
+        ) -> nalgebra::SVector<num_dual::DualSVec64<3>, 2> {
         let x = params[0];
         let y = params[1];
         let z = params[2];
-        *residual = Matrix2x1d::new(x + 2.0 * y + 4.0 * z, y * z);
-        if let Some(jacobian) = jacobian {
-            *jacobian = Matrix2x3d::new(1.0, 2.0, 4.0, 0.0, z, y);
-        }
+        return nalgebra::SVector::from([x + y.mul(2.0) + z.mul(4.0), y * z]);
     }
 }
 fn main() {
     let mut x0 = Matrix3x1d::new(0.76026643, -30.01799744, 0.55192142);
     ExampleStatic::solve(&mut x0);
+    println!("auto grad: {}", x0);
+    println!("residaul: {}", ExampleStatic::cost_function(x0.map(num_dual::DualSVec64::from_re)));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate nalgebra as na;
 use na::*;
-use nalgebra::allocator::Allocator;
 use num_dual::*;
 // use num_dual::{jacobian, DualSVec64, DualNum};
 
@@ -22,25 +21,7 @@ fn print_type_of<T>(_: &T) {
 
 fn main() {
     println!("testttt");
-    // let mut x0 = mat![[0.76026643], [-30.01799744], [0.55192142]];
-    // let x = x0.read(0, 0);
-    // let y = x0.read(1, 0);
-    // let z = x0.read(2, 0);
-    // let residual = mat![[x + 2.0 * y + 4.0 * z], [y * z]];
-    // let jacobian = mat![[1.0, 2.0, 4.0], [0.0, z, y]];
-    // println!("{}", jacobian.ncols());
-    // let xvec = na::SMatrix::<num_dual::Dual64, 3, 1>::new(
-    //     num_dual::Dual::from(1.0),
-    //     num_dual::Dual::from(1.0),
-    //     num_dual::Dual::from(1.0),
-    // );
     let xy = na::SVector::from([5.0, 3.0, 2.0]);
-    // let fun = |xy: na::SVector<num_dual::DualSVec64<3>, 3>| {
-    //     na::SVector::from([
-    //         xy[0] * xy[1].powi(3) * xy[2],
-    //         xy[0].powi(2) * xy[1] * xy[2].powi(2),
-    //     ])
-    // };
     let (f, jac) = num_dual::jacobian(f2, xy);
     println!("{f} {jac}");
     println!("{}", f2(xy.map(DualVec::from_re)));
