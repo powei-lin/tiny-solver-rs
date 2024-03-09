ea4b60208714622193b449671259ef8b5977068e
Powei Lin <poweilin1994@gmail.com>
2024-03-08 22:13:48 -0500

    param with dual

--- examples/test_residual_block.rs
+++ examples/test_residual_block.rs
@@ -3,6 +3,7 @@ use std::vec;
 
 extern crate nalgebra as na;
 use na::{Const, Dyn};
+use num_dual::{DualDVec64, DualVec};
 use tiny_solver::problem;
 
 fn cost_function_dyn(
@@ -12,6 +13,18 @@ fn cost_function_dyn(
     let y = &params[0][1];
     let z = &params[0][2];
     return na::dvector![x + y.clone().mul(2.0) + z.clone().mul(4.0), y * z];
+}
+
+fn rows(aa: &Vec<i32>) -> Vec<Vec<i32>> {
+    let mut result = Vec::new();
+    let mut current = 0;
+    for &num in aa {
+        let next = current + num;
+        let range = (current..next).collect::<Vec<_>>();
+        result.push(range);
+        current = next;
+    }
+    result
 }
 
 fn main() {
@@ -39,4 +52,19 @@ fn main() {
     println!("{}", a[1].eps.clone().unwrap_generic(Dyn(3), Const::<1>));
     let (r, j) = rsb.jacobian(&vec![na::dvector![1.0], na::dvector![1.0, 1.0]]);
     println!("{},{}", r, j);
+    let vv = vec![na::dvector![1.0], na::dvector![1.0, 1.0]];
+    let ff = na::dvector![1.0, 1.0, 2.0];
+    // let abc: Vec<DualDVec64> = ff.row_iter().map(|x| num_dual::DualDVec64::new(x[0], num_dual::Derivative::some(na::dvector![0.0, 0.0, 1.0, 0.0]))).collect();
+    let idm = na::DMatrix::<f64>::identity(3, 3);
+    let abc = na::DVector::from_row_iterator(
+        3,
+        ff.row_iter().enumerate().map(|(i, x)| {
+            num_dual::DualDVec64::new(
+                x[0],
+                num_dual::Derivative::some(na::DVector::from(idm.column(i))),
+            )
+        }),
+    );
+    println!("{}", abc);
+    let aa = vec![1, 2, 3, 4];
 }
--- src/problem.rs
+++ src/problem.rs
@@ -19,11 +19,36 @@ pub struct ResidualBlock {
 }
 impl ResidualBlock {
     pub fn jacobian(self, params: &Vec<na::DVector<f64>>) -> (na::DVector<f64>, na::DMatrix<f64>) {
-        let dim_variable = params.iter().fold(0, |acc, x| acc + x.shape().0);
-        println!("dim_variable {}", dim_variable);
+        let variable_rows: Vec<usize> = params.iter().map(|x| x.shape().0).collect();
+        let dim_variable = variable_rows.iter().fold(0, |acc, x| acc + x);
+        let variable_row_idx_vec = get_variable_rows(&variable_rows);
+        let indentity_mat = na::DMatrix::<f64>::identity(dim_variable, dim_variable);
+        let params_with_dual: Vec<na::DVector<num_dual::DualDVec64>> = params
+            .iter()
+            .enumerate()
+            .map(|(i, param)| {
+                na::DVector::from_row_iterator(
+                    param.nrows(),
+                    param.row_iter().enumerate().map(|(j, x)| {
+                        num_dual::DualDVec64::new(
+                            x[0],
+                            num_dual::Derivative::some(na::DVector::from(
+                                indentity_mat.column(variable_row_idx_vec[i][j]),
+                            )),
+                        )
+                    }),
+                )
+            })
+            .collect();
+        println!("{:?}", params_with_dual);
+        // params.iter().enumerate()
+        let x0 = num_dual::DualDVec64::new(
+            1.2,
+            num_dual::Derivative::some(na::dvector![1.0, 0.0, 0.0, 1.0]),
+        );
         (
             na::DVector::<f64>::zeros(self.dim_residual),
-            na::DMatrix::<f64>::zeros(self.dim_residual, self.dim_residual),
+            na::DMatrix::<f64>::zeros(self.dim_residual, dim_variable),
         )
     }
 }
@@ -35,3 +60,15 @@ impl ResidualBlock {
 //     _params: na::SVector<num_dual::DualSVec64<NUM_PARAMETERS>, NUM_PARAMETERS>,
 // ) -> na::SVector<num_dual::DualSVec64<NUM_PARAMETERS>, NUM_RESIDUALS>
 // }
+
+fn get_variable_rows(variable_rows: &[usize]) -> Vec<Vec<usize>> {
+    let mut result = Vec::new();
+    let mut current = 0;
+    for &num in variable_rows {
+        let next = current + num;
+        let range = (current..next).collect::<Vec<_>>();
+        result.push(range);
+        current = next;
+    }
+    result
+}
