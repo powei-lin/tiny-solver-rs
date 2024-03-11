b4f1da08603a29a999cd4d8a944f803959526c8d
Powei Lin <poweilin1994@gmail.com>
2024-03-11 17:28:08 -0400

    linear

---
+++ src/linear.rs
@@ -0,0 +1,25 @@
+use faer::prelude::SpSolver;
+use std::ops::Mul;
+
+pub fn sparse_cholesky(
+    residuals: &faer::Mat<f64>,
+    jacobians: &faer::sparse::SparseColMat<usize, f64>,
+) -> faer::prelude::Mat<f64> {
+    let hessian = jacobians
+        .as_ref()
+        .transpose()
+        .to_col_major()
+        .unwrap()
+        .mul(jacobians.as_ref());
+    let b = jacobians.as_ref().transpose().mul(-residuals);
+    let dx = hessian.sp_cholesky(faer::Side::Lower).unwrap().solve(b);
+    dx
+}
+
+pub fn sparse_qr(
+    residuals: &faer::Mat<f64>,
+    jacobians: &faer::sparse::SparseColMat<usize, f64>,
+) -> faer::prelude::Mat<f64> {
+    let dx = jacobians.sp_qr().unwrap().solve(-residuals);
+    dx
+}
