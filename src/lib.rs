8aeded9d2f3c5a61736aba990a82e6c6120ccfe7
Powei Lin <poweilin1994@gmail.com>
2024-03-11 18:09:03 -0400

    add py

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod gauss_newton_optimizer;
 pub mod linear;
 pub mod optimizer;
 pub mod problem;
+pub mod python;
 pub mod residual_block;
 pub mod tiny_solver;
+
 pub use crate::tiny_solver::*;
