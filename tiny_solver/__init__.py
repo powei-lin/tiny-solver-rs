e6b82ae1dfa39ff6c8a00242e4b4c7884da2c89e
Powei Lin <poweilin1994@gmail.com>
2024-03-15 14:01:38 -0400

    add pyi

---
+++ tiny_solver/__init__.py
@@ -0,0 +1,1 @@
+from .tiny_solver import *
---
+++ tiny_solver/factors/__init__.pyi
@@ -0,0 +1,7 @@
+import numpy as np
+
+class CostFactorSE2:
+    def __init__(self, x: float, y: float, theta: float) -> None: ...
+
+class BetweenFactor:
+    def __init__(self, x: np.ndarray, /) -> None: ...
