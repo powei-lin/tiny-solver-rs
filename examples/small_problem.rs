e10f15773dfbe3276e99a234701c056f2a4ff6cd
Powei Lin <poweilin1994@gmail.com>
2024-03-24 11:30:39 -0400

    add example

---
+++ examples/small_problem.rs
@@ -0,0 +1,64 @@
+use std::collections::HashMap;
+use std::ops::Mul;
+
+use nalgebra as na;
+use tiny_solver::{self, Optimizer};
+
+#[derive(Clone)]
+struct CustomFactor {}
+
+impl tiny_solver::factors::Factor for CustomFactor {
+    fn residual_func(
+        &self,
+        params: &[nalgebra::DVector<num_dual::DualDVec64>],
+    ) -> nalgebra::DVector<num_dual::DualDVec64> {
+        let x = &params[0][0];
+        let y = &params[1][0];
+        let z = &params[1][1];
+
+        na::dvector![x + y.clone().mul(2.0) + z.clone().mul(4.0), y.mul(z)]
+    }
+}
+
+fn main() {
+    // init logger, `export RUST_LOG=trace` to see more log
+    env_logger::init();
+
+    // init problem (factor graph)
+    let mut problem = tiny_solver::Problem::new();
+
+    // add residual blocks (factors)
+    // add residual x needs to be close to 3.0
+    problem.add_residual_block(
+        1,
+        vec![("x".to_string(), 1)],
+        Box::new(tiny_solver::factors::PriorFactor {
+            v: na::dvector![3.0],
+        }),
+        None,
+    );
+    // add custom residual for x and yz
+    problem.add_residual_block(
+        2,
+        vec![("x".to_string(), 1), ("yz".to_string(), 2)],
+        Box::new(CustomFactor {}),
+        None,
+    );
+
+    // the initial values for x is 0.7 and yz is [-30.2, 123.4]
+    let initial_values = HashMap::<String, na::DVector<f64>>::from([
+        ("x".to_string(), na::dvector![0.7]),
+        ("yz".to_string(), na::dvector![-30.2, 123.4]),
+    ]);
+
+    // initialize optimizer
+    let optimizer = tiny_solver::GaussNewtonOptimizer {};
+
+    // optimize
+    let result = optimizer.optimize(&problem, &initial_values, None);
+
+    // result
+    for (k, v) in result {
+        println!("{}: {}", k, v);
+    }
+}
