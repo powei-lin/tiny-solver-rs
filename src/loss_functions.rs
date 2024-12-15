c1b0e41832a574900d86ce5e2570a74f2eb3bb61
Powei Lin <poweilin1994@gmail.com>
2024-12-15 16:05:11 -0500

    cauchy loss

--- src/loss_functions.rs
+++ src/loss_functions.rs
@@ -39,3 +39,29 @@ impl Loss for HuberLoss {
         }
     }
 }
+
+pub struct CauchyLoss {
+    scale2: f64,
+    c: f64,
+}
+impl CauchyLoss {
+    pub fn new(scale: f64) -> Self {
+        let scale2 = scale * scale;
+        CauchyLoss {
+            scale2,
+            c: 1.0 / scale2,
+        }
+    }
+}
+impl Loss for CauchyLoss {
+    fn evaluate(&self, s: f64) -> [f64; 3] {
+        let sum = 1.0 + s * self.c;
+        let inv = 1.0 / sum;
+        // 'sum' and 'inv' are always positive, assuming that 's' is.
+        [
+            self.scale2 * sum.log2(),
+            inv.max(f64::MIN),
+            -self.c * (inv * inv),
+        ]
+    }
+}
