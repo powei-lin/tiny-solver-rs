c7b750545ba51f3b15d102746fcb7a3ac2540ded
Hossam Rabbouh <hrabbouh@Hossams-MacBook-Pro-2.local>
2024-12-21 12:07:29 -0500

    Test update

--- tests/test_loss.rs
+++ tests/test_loss.rs
@@ -10,7 +10,16 @@ mod tests {
 
         let arctan_loss = ArctanLoss::new(tolerance);
         
-        assert!(arctan_loss.evaluate(1.0)[0] < arctan_loss.evaluate(30.0)[0]);
+        let rho1 = arctan_loss.evaluate(1.0);
+        let rho2 = arctan_loss.evaluate(30.0);
+
+        // Test that rho[0] grows linearly with the scale
+        assert!(rho1[0] < rho2[0]);
+
+        // Test that rho[1] decreases linearly with the scale
+        assert!(rho1[1] > rho2[1]);
+
+        // Test that scales largely above the tolerance are asymptotically bounded
         assert!(arctan_loss.evaluate(tolerance * tolerance * tolerance)[0] < asymptote);
     }
 }
